4cbc5df80df6d16f3d751bf8cf03b0c46fdc5828
Alex <cigan@ua.fm>
2021-03-16 20:37:05 +0200

    feat: add a solution of the task nthFibo

---
+++ stage-1/indexFibo.js
@@ -0,0 +1,20 @@
+function nthFibo(n) {
+    const fibo = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309, 3524578, 5702887, 9227465, 14930352, 24157817, 39088169, 63245986, 102334155];
+    return fibo[n - 1];
+}
+
+console.log(nthFibo(1))
+
+
+/*
+I love Fibonacci numbers in general, but I must admit I love some more than others.
+
+I would like for you to write me a function that when given a number (n) returns the n-th number in the Fibonacci Sequence.
+
+For example:
+
+   nthFibo(4) == 2
+Because 2 is the 4th number in the Fibonacci Sequence.
+
+For reference, the first two numbers in the Fibonacci sequence are 0 and 1, and each subsequent number is the sum of the previous two.
+*/
